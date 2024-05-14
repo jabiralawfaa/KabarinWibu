a4ea1f609f35e03c16d646ffd9a697a1d25d7613
jabiralawfaa <162452844+jabiralawfaa@users.noreply.github.com>
2024-05-14 11:45:39 +0700

    KabarinWibu

---
+++ script.js
@@ -0,0 +1,171 @@
+// navbar fixed
+window.onscroll = function () {
+  const header = document.querySelector("header");
+  const fixednav = header.offsetTop;
+  const backtop = document.querySelector("#backtop");
+
+  if (window.pageYOffset > fixednav) {
+    header.classList.add("navbar-fixed");
+    backtop.classList.remove("hidden");
+  } else {
+    header.classList.remove("navbar-fixed");
+    backtop.classList.add("hidden");
+  }
+};
+
+// hamburger
+const hamburger = document.querySelector("#hamburger");
+const navmenu = document.querySelector("#nav-menu");
+
+hamburger.addEventListener("click", function () {
+  hamburger.classList.toggle("hamburger-active");
+  navmenu.classList.toggle("hidden");
+});
+
+// click luar hamburger
+window.addEventListener("click", function (e) {
+  if (e.target != hamburger && e.target != navmenu) {
+    hamburger.classList.remove("hamburger-active");
+    navmenu.classList.add("hidden");
+  }
+});
+
+// search bar
+const inputBox = document.getElementById("input-box");
+const resultBox = document.getElementById("result-box");
+const judulBlogs = document.querySelectorAll("#judulBlog");
+
+// Add an event listener to the input box
+inputBox.addEventListener("input", (e) => {
+  const searchTerm = e.target.value.toLowerCase();
+  const matchingElements = [];
+
+  // Loop through all the a elements and check if the text matches
+  judulBlogs.forEach((element) => {
+    const text = element.textContent.toLowerCase();
+    if (text.includes(searchTerm)) {
+      matchingElements.push(element);
+    }
+  });
+
+  // Clear the result box and append the matching elements (up to 5 elements)
+  resultBox.innerHTML = "";
+  const maxResults = Math.min(matchingElements.length, 5);
+  for (let i = 0; i < maxResults; i++) {
+    resultBox.appendChild(matchingElements[i].cloneNode(true));
+  }
+});
+
+// Blog sistem
+const showAllBlogButton = document.getElementById("showAllBlog");
+const allBlogDiv = document.getElementById("allBlog");
+
+let isShowingAll = false;
+
+showAllBlogButton.addEventListener("click", () => {
+  isShowingAll = !isShowingAll;
+  allBlogDiv.classList.toggle("show-all", isShowingAll);
+  showAllBlogButton.textContent = isShowingAll ? "Tutup Semua blog" : "Tampilkan semua blog";
+});
+
+// Pagination
+// let currentPage = 1;
+// const itemsPerPage = 3;
+// const allItems = document.querySelectorAll(".blogCard");
+// const totalPages = Math.ceil(allItems.length / itemsPerPage);
+
+// function showPage(pageNumber) {
+//   const startIndex = (pageNumber - 1) * itemsPerPage;
+//   const endIndex = startIndex + itemsPerPage;
+//   allItems.forEach((item, index) => {
+//     if (index >= startIndex && index < endIndex) {
+//       item.classList.remove("hidden");
+//       item.classList.add("block");
+//     } else {
+//       item.classList.add("hidden");
+//       item.classList.remove("block");
+//     }
+//   });
+//   document.getElementById("page-info").textContent = `${pageNumber} / ${totalPages}`;
+// }
+
+// function changePage(direction) {
+//   const newPage = currentPage + direction;
+//   if (newPage > 0 && newPage <= totalPages) {
+//     currentPage = newPage;
+//     showPage(currentPage);
+//   }
+// }
+
+// document.addEventListener("DOMContentLoaded", () => {
+//   showPage(1);
+//   document.querySelector(".page-link.prev").addEventListener("click", () => changePage(-1));
+//   document.querySelector(".page-link.next").addEventListener("click", () => changePage(1));
+// });
+
+// my blog system
+// document.addEventListener("DOMContentLoaded", function () {
+//   const blogCards = document.querySelectorAll(".blogCard");
+//   const showMoreButton = document.getElementById("showMore");
+//   const closeCardsButton = document.getElementById("closeCards");
+//   let visibleCards = 3;
+
+//   // Fungsi untuk menampilkan blog cards
+//   function updateDisplay() {
+//     blogCards.forEach((card, index) => {
+//       if (index < visibleCards) {
+//         card.classList.remove("hidden");
+//         card.classList.add("block");
+//       } else {
+//         card.classList.add("hidden");
+//         card.classList.remove("block");
+//       }
+//     });
+//   }
+
+//   // Event listener untuk tombol show
+//   showMoreButton.addEventListener("click", () => {
+//     visibleCards = Math.min(blogCards.length, visibleCards + 3); // Tambahkan 3 kartu lagi
+//     updateDisplay();
+//   });
+
+//   // Event listener untuk tombol close
+//   closeCardsButton.addEventListener("click", () => {
+//     visibleCards = 3; // Reset ke hanya 3 kartu yang ditampilkan
+//     updateDisplay();
+//   });
+
+//   updateDisplay(); // Panggil pada saat load untuk setup awal
+// });
+
+// dark toggle
+const dtoggle = document.querySelector("#dtoggle");
+const ltoggle = document.querySelector("#ltoggle");
+const backli = document.querySelector("#backiconlight");
+const backda = document.querySelector("#backicondark");
+const darkToggle = document.querySelector("#dark-toggle");
+const html = document.querySelector("html");
+
+darkToggle.addEventListener("click", function () {
+  if (darkToggle.checked) {
+    html.classList.add("dark");
+
+    localStorage.theme = "dark";
+  } else {
+    html.classList.remove("dark");
+
+    localStorage.theme = "light";
+  }
+
+  if (localStorage.theme === "dark") {
+    dtoggle.classList.remove("hidden");
+    ltoggle.classList.add("hidden");
+    backda.classList.remove("hidden");
+    backli.classList.add("hidden");
+  } else {
+    dtoggle.classList.add("hidden");
+    ltoggle.classList.remove("hidden");
+    backda.classList.add("hidden");
+    backli.classList.remove("hidden");
+  }
+});
